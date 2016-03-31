c16ac8c3ca8c26bdd0efd10420a40da7d722109e
madebyeggs <madebyeggs@gmail.com>
2016-03-31 22:23:06 +0100

    fonts called from assets

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -4,19 +4,34 @@ class SlidesController < ApplicationController
     define_slides
     set_meta_tags :og => {
       :url => "#{@currentUrl}",
-      :title    => 'Split Music | News',
-      :description => '21st Century Music Publishing | Unique Music to Picture'
+      :title    => 'Elephant Music | News',
+      :description => 'Unique Music to Picture'
     }
     set_meta_tags twitter: {
       card: "summary_large_image",
       site: "@fairsplitmusic",
-      title: "Split Music | News",
-      description: "21st Century Music Publishing | Unique Music to Picture"
+      title: "Elephant Music | News",
+      description: "Unique Music to Picture"
     }
   end
   
   def show
     @slide = Announcement.find_by_slug(params[:id]) || Artist.find_by_slug(params[:id]) || Work.find_by_slug(params[:id]) || Release.find_by_slug(params[:id])
+    set_meta_tags og: {
+      url: "#{@currentUrl}",
+      image: "#{@slide.fb_image}",
+      title: "#{@slide.slide_title}",
+      description: "#{@slide.description}",
+      type: "musician"
+    }
+    set_meta_tags twitter: {
+      card: "summary_large_image",
+      site: "",
+      title: "#{@slide.slide_title}",
+      description: "#{@slide.description}",
+      image: "#{@slide.fb_image}"
+    }
+    render :show, flush: true
   end
   
 end
